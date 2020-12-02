43dc40b0514275995fb6a7440eb00e757a7d3e90
Sampada-VS <samssv1@gmail.com>
2020-12-02 08:57:25 +0530

    Added pojo class to create contacts in addressbook

---
+++ src/main/java/com/addressbookworkshop/AddressBookData.java
@@ -0,0 +1,24 @@
+package com.addressbookworkshop;
+
+public class AddressBookData {
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String state;
+	public String zip;
+	public String phone;
+	public String email;
+
+	public AddressBookData(String firstName, String lastName, String address, String city, String state,
+						   String zip, String phone, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phone = phone;
+		this.email = email;
+	}
+}
